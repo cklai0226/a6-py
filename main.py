df26949fce04efa895d0c6451c5c2418d0e40674
Paulo Rodrigues <paulorodriguexv@gmail.com>
2016-12-11 21:51:00 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,125 @@
+import time
+import Queue
+from observer import Observable, Observer
+from threading import Thread
+import serial
+
+# self.serialport.write('ath\r');
+# time.sleep(2)
+# self.serialport.write('atd33443744;\r')
+# self.serialport.write('AT\r')
+# self.serialport.write('ATI\r')
+# self.serialport.write('AT+CNUM\r')
+# self.serialport.write('AT+CCID\r')
+
+# self.serialport.write('AT+CREG?\r')
+# self.serialport.write('AT+CSQ\r')
+
+class SmsSendCommand(object):
+
+    """
+    Class to send SMS on A6 gsm/gprs chip
+    """
+
+    def __init__(self, port):
+
+        self.serialport = port
+
+    def send(self):
+        """
+        send sms
+        """
+
+        self.serialport.write('at+cmgf=1\r')
+        time.sleep(2)
+
+        self.serialport.write('at+cmgs=\"999349859"')
+        self.serialport.write('\r\n')
+        time.sleep(2)
+
+        self.serialport.write('GSM A6 test mesage!')
+        time.sleep(0.5)
+
+        self.serialport.write("\x1a")
+
+class A6Command(Observer):
+    """
+      A6 Base Command Class
+    """
+    pass
+
+class SMSNewShortMessage(A6Command):
+    """
+      SMS Command Class
+    """
+    SMS_TEXT = "+CMT"
+
+    def __init__(self):
+        self.init = False
+        self.queue = Queue.Queue(2)
+
+    def update(self, observable, arg):
+        if arg[:4] == self.SMS_TEXT:
+            self.init = True
+
+        if self.init:
+            self.queue.put(arg)
+
+        if self.queue.full() is False:
+            return
+
+        try:
+            while not self.queue.empty():
+                pdu_message = self.queue.get()
+
+            print pdu_message
+        finally:
+            self.init = False
+
+
+
+serialcom = serial.Serial()
+
+serialcom.port = '/dev/ttyAMA0'
+serialcom.baudrate = 115200
+serialcom.parity = serial.PARITY_NONE
+serialcom.stopbits = serial.STOPBITS_ONE
+serialcom.bytesize = serial.EIGHTBITS
+
+serialcom.open()
+
+serialcom.reset_input_buffer()
+serialcom.reset_output_buffer()
+
+class A6Communication(Thread, Observable):
+    """Threaded A6 communication """
+    def __init__(self, comm):
+        super(A6Communication, self).__init__()
+        Observable.__init__(self)
+
+        self.comm = comm
+
+    def run(self):
+        while True:
+            mes = self.comm.readline()
+
+            self.setChanged()
+            self.notify(mes)
+
+t = A6Communication(serialcom)
+t.register(SMSNewShortMessage())
+
+t.setDaemon(True)
+t.start()
+
+while True:
+    time.sleep(3)
+print "Exiting Main Thread"
+
+# self.serialport.write('AT')
+# time.sleep(3)
+
+# while 1:
+#  self.serialport.write('AT')
+#  x=self.serialport.read()
+
